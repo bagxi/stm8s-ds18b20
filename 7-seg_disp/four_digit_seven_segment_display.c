fafd77175f12e6a53c2ee6e9b0bf2264c099f0d7
bagxi <bagelwithraisins@gmail.com>
2016-12-01 13:51:02 +0300

    changed head-part of each file of this branch

--- 7-seg_disp/four_digit_seven_segment_display.c
+++ 7-seg_disp/four_digit_seven_segment_display.c
@@ -1,5 +1,18 @@
-// Copyright (c) 2016 bagXi. Contacts: <imcodelike@gmail.com>
-// License: https://www.gnu.org/licenses/gpl-2.0
+// Copyright (c) 2016 bagXi. All rights reserved.
+//
+// Licensed under the GNU General Public License, Version 2.0
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation; version 2 of the License.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, see <https://www.gnu.org/licenses/gpl-2.0>
 
 
 #include "four_digit_seven_segment_display.h"
